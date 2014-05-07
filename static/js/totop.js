0a200453a0e07cdc29d8ac3263d4c959179c469c
Риточка <nyakiss@yandex.ru>
2014-05-07 23:28:33 +0700

    add static

---
+++ static/js/totop.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 500) {
+			$('.to-top').fadeIn();
+		} else {
+			$('.to-top').fadeOut();
+		}
+	});
+
+	$('.to-top').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+});
---
+++ static/js/youtube.js
@@ -0,0 +1,21 @@
+﻿var width = 584
+var height = 329
+
+String.prototype.format = function() {
+	var formatted = this;
+	for (var i = 0; i < arguments.length; i++) {
+		var regexp = new RegExp('\\{'+i+'\\}', 'gi');
+		formatted = formatted.replace(regexp, arguments[i]);
+	}
+	return formatted;
+};
+
+$(function() {
+	$(".youtube-preview").on("click", function(e) {
+		var iframe = '<iframe class="youtube-player" type="text/html" ' +
+					 'width="{0}" height="{1}" ' +
+					 'src="http://www.youtube.com/embed/{2}?autoplay=1" ' +
+					 'frameborder="0"></iframe>';
+		$(this).replaceWith(iframe.format(width, height, $(this).attr("id")));
+	});
+});
